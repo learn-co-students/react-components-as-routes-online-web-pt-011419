f70544dbc7a9a701994f1a38c06b6ac3ba16d16a
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-10-19 12:33:33 -0400

    Done.

---
+++ src/SignUp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+  render() {
+    return (
+        <form>
+          <h1>Sign Up Ya Bish</h1>
+          <div>
+            <input type="email" name="email" placeholder="email" />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+            <label htmlFor="username">Username</label>
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+            <label htmlFor="password">Password</label>
+            </div>
+            <input type="submit" value="Login" />
+         </form>
+       )}}
+
+export default SignUp;
